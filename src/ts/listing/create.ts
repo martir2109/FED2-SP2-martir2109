42ce09fafad3058fbac47b8aa5d5c57b32687548
Martine R Karlsen <martinekar08@gmail.com>
2025-11-23 14:18:47 +0100

    Create listing form submission with validation
    - Get the create-listing-form by id
    - Prevent the from from submitting when refreshing
    - Get the accessToken and apiKey from localStorage
    - Check to see if there is an accessToken (must be logged in to create a listing). Direct user to login if there is not accessToken.
    - Get the input values from the input fields
    - Input validation, no fields can be empty + validation for valid image url and endsAt date
    - Create a new object called “Listing”, containing the input values
    - Send POST to the Noroff API to create a new listing
    - Alert the user if the listing has been successfully created, otherwise show error alert
    - Reset the form
    - Direct the user to landing/ home page

---
+++ src/ts/listing/create.ts
@@ -0,0 +1,144 @@
+import {
+  showError,
+  clearError,
+  attachInputListeners,
+  getAuthenticationCredentials,
+} from "../utils.ts";
+
+import {
+  API_BASE_URL,
+  API_ENDPOINTS,
+  API_Headers_accesstoken_content_apikey,
+} from "../apiConfig.js";
+
+const createListingForm = document.getElementById(
+  "create-listing-form",
+) as HTMLFormElement;
+
+createListingForm.addEventListener("submit", async function (event) {
+  event.preventDefault();
+
+  const { accessToken, apiKey } = getAuthenticationCredentials();
+
+  if (!accessToken) {
+    alert("You must be logged in to create a listing.");
+    window.location.href = "../../auth/login/index.html";
+    return;
+  }
+
+  const title = (document.getElementById("title") as HTMLInputElement).value;
+  const description = (
+    document.getElementById("description") as HTMLTextAreaElement
+  ).value;
+  const tags = (document.getElementById("tags") as HTMLInputElement).value;
+  const media = {
+    url: (document.getElementById("media") as HTMLInputElement).value,
+    alt: (document.getElementById("alt") as HTMLInputElement).value,
+  };
+  const endAtInput = (document.getElementById("endsAt") as HTMLInputElement)
+    .value;
+  const endsAt = new Date(endAtInput);
+
+  let hasError = false;
+
+  if (!title.trim()) {
+    showError("title", "Title cannot be empty.");
+    hasError = true;
+  } else {
+    clearError("title");
+  }
+
+  if (!description.trim()) {
+    showError("description", "Title cannot be empty.");
+    hasError = true;
+  } else {
+    clearError("description");
+  }
+
+  if (!tags || tags.length === 0) {
+    showError("tags", "Tags cannot be empty.");
+    hasError = true;
+  } else {
+    clearError("tags");
+  }
+
+  if (!media.url.trim() || !media.url.trim().startsWith("http")) {
+    showError(
+      "media",
+      "Please enter a valid image url that starts with http or https.",
+    );
+    hasError = true;
+  } else {
+    clearError("media");
+  }
+
+  if (!media.alt.trim()) {
+    showError("alt", "Alt text cannot be empty.");
+    hasError = true;
+  } else {
+    clearError("alt");
+  }
+
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+
+  if (!endsAt) {
+    showError("endsAt", "End date cannot be empty.");
+    hasError = true;
+  } else if (isNaN(endsAt.getTime()) || endsAt < today) {
+    showError("endsAt", "The date is invalid.");
+    hasError = true;
+  } else {
+    clearError("endsAt");
+  }
+
+  if (hasError) return;
+
+  const tagsArray = tags
+    .split(",")
+    .map((tag) => tag.trim())
+    .filter((tag) => tag.length > 0);
+
+  const listing = {
+    title,
+    description: description,
+    tags: tagsArray,
+    media: [media],
+    endsAt: endsAt.toISOString(),
+  };
+
+  try {
+    const response = await fetch(
+      `${API_BASE_URL}${API_ENDPOINTS.AUCTION.LISTINGS}`,
+      {
+        method: "POST",
+        headers: API_Headers_accesstoken_content_apikey(accessToken, apiKey),
+        body: JSON.stringify(listing),
+      },
+    );
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      const errorMessage =
+        data.errors?.[0]?.message || `HTTP ${response.status}`;
+      throw new Error(errorMessage);
+    }
+
+    alert("Listing successfully created!");
+    createListingForm.reset();
+    window.location.href = "/index.html";
+  } catch (error) {
+    const errorMessage = error.message || "An undexpected error occured.";
+    alert("Something went wrong while creating the post: " + errorMessage);
+  }
+});
+
+attachInputListeners([
+  "#title",
+  "#description",
+  "#tags",
+  "#media",
+  "#alt",
+  "#endsAt",
+]);
