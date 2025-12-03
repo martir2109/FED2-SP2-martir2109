aa5ec992afd2e0134ca87cece9a112d84f752048
Martine R Karlsen <martinekar08@gmail.com>
2025-12-03 23:02:20 +0100

    Updated vite config with multi page

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,20 @@ export default defineConfig({
   server: {
     port: 5501,
   },
+  build: {
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+        login: "./auth/login/index.html",
+        register: "./auth/register/index.html",
+        about: "./footer-links/about/index.html",
+        privacyPolicy: "./footer-links/privacy-policy/index.html",
+        termsOfService: "./footer-links/terms-of-service/index.html",
+        createListing: "./listing/create/index.html",
+        editListing: "./listing/edit/index.html",
+        viewListing: "./listing/view/index.html",
+        profile: "./profile/index.html",
+      },
+    },
+  },
 });
