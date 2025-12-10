031ded24775a422e0b609a581c34cb6089afe75a
Martine R Karlsen <martinekar08@gmail.com>
2025-12-10 23:11:40 +0100

    changed color on toggle

--- src/ts/landing/filteringContent.ts
+++ src/ts/landing/filteringContent.ts
@@ -44,7 +44,7 @@ export function createFilteringContent(): string {
               <input type="checkbox" class="sr-only peer" id="active-toggle" checked/>
               <div
                 class="group peer ring-0 bg-gray-400 rounded-full outline-none duration-300 after:duration-300
-                 w-22 h-10 shadow-md peer-checked:bg-emerald-500 peer-focus:outline-none after:content-['✖️'] 
+                 w-22 h-10 shadow-md peer-checked:bg-green peer-focus:outline-none after:content-['✖️'] 
                  after:rounded-full after:absolute after:bg-gray-50 after:outline-none after:h-8 after:w-8 
                  after:top-1 after:left-1 after:-rotate-180 after:flex after:justify-center after:items-center 
                  peer-checked:after:translate-x-12 peer-checked:after:content-['✔️'] peer-hover:after:scale-95 peer-checked:after:rotate-0"
